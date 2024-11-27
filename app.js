2615d0bf7b79217d1e39885f23e094c00d2b4a7b
Naveen-1019 <chinthalacheruvunaveen@10gmail.com>
2024-11-27 12:35:26 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+
+// let heading = document.getElementsByTagName('h1');
+// let paragraph = document.getElementsByClassName('paragraph')
+// let link = document.getElementById('link')
+
+
+// console.log(heading)
+
+// console.log(paragraph)
+
+// console.log(link)
+
+// let heading = document.querySelectorAll('h1');
+
+// console.log(heading)
+
+// let parent = document.querySelector('.container')
+
+// console.log(parent)
